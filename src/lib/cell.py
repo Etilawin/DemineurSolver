aa6d2637c3094889d85841ec65d6fcdc7b252c8a
Etilawin <kim.vallee@protonmail.com>
2019-10-15 13:18:17 +0200

    Started coding with pycharm and added some efficiency when creating the board

--- src/lib/cell.py
+++ src/lib/cell.py
@@ -1,17 +1,36 @@
 class Cell:
-    def __init__(self):
+    def __init__(self, x, y, size):
         self.bomb = False
         self.flag = False
+        self.revealed = False
+        self.neighbours = []
+        self.nb_neighbours_bombs = 0
 
-    def setBomb(self):
+    def set_bomb(self):
         self.bomb = True
 
-    def isBomb(self):
+    def is_bomb(self):
         return self.bomb
 
-    def setFlag(b):
+    def set_flag(self, b):
         assert isinstance(b, bool), "setFlag: Cell: b is not a boolean"
         self.flag = b
 
-    def isFlagged():
+    def is_flagged(self):
         return self.flag
+
+    def set_neighbours(self, neighbours):
+        self.neighbours = neighbours
+        self.nb_neighbours_bombs = sum([c.is_bomb() for c in self.get_neighbours()])
+
+    def get_neighbours(self):
+        return self.neighbours
+
+    def __str__(self):
+        if self.flag:
+            return "!"
+        elif self.revealed:
+            if self.bomb:
+                return "o"
+            return "{}".format(self.nb_neighbours_bombs)  #  if self.nb_neighbours_bombs != 0 else " "
+        return " "
--- src/lib/demineur.py
+++ src/lib/demineur.py
@@ -1,24 +1,51 @@
-from Cell import Cell
+from .cell import Cell
 from random import randint, random
 
+
 class Demineur:
-    def __init__(self, size, nbBombs):
+    def __init__(self, size, nb_bombs):
         assert isinstance(size, tuple), "__init__: Demineur: size is not a tuple"
         assert len(size) == 2, "__init__: Demineur: size is not of size 2"
-        assert isinstance(nbBombs, int), "__init__: Demineur: nbBombs is not an int"
-        assert nbBombs < size[0] * size[1]
+        assert isinstance(nb_bombs, int), "__init__: Demineur: nbBombs is not an int"
+        assert nb_bombs < size[0] * size[1]
         self.width, self.height = size
-        self.nbBombs = nbBombs
-        this.board = [[Cell() for y in range(size[1])] for x in range(size[0])]
-        this.__initBoard()
+        self.nbBombs = nb_bombs
+        self.board = [[Cell(size, x, y) for y in range(size[1])] for x in range(size[0])]
+        self.__create_neighbours()
+        self.__init_board()
 
-    def __initBoard(self):
+    def __create_neighbours(self):
+        for x in range(self.width):
+            for y in range(self.height):
+                neighbours = [self.board[x+dx][y+dy] for dx in [-1, 0, 1] for dy in [-1, 0, 1]
+                              if 0 <= x+dx <= self.width - 1 and 0 <= y + dy <= self.height - 1]
+                self.board[x][y].set_neighbours(neighbours)
+
+    def __init_board(self):
         # Random generator of bombs
-        bombsToPlace = self.nbBombs
-        while bombsToPlace != 0:
-            placed = self.placeBomb()
+        bombs_to_place = self.nbBombs
+        while bombs_to_place != 0:
+            x = randint(self.width)
+            y = randint(self.height)
+            placed = self.__place_bomb(x, y)
             if placed:
-                bombsToPlace -= 1
+                bombs_to_place -= 1
 
-    def placeBomb(self):
-        pass
+    def __place_bomb(self, x, y):
+        if self.board[x][y].is_bomb():
+            return False
+
+        self.board[x][y].set_bomb()
+        return True
+
+    def __str__(self):
+        ret = ""
+        for x in range(self.width):
+            ret += "*-"*self.width + "*\n"
+            for y in range(self.height):
+                ret += "|{}|".format(self.board[x][y])
+            ret += "\n"
+        return ret + "*-"*self.width + "*"
+
+
+
